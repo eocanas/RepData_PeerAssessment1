9ad4dfbcec4937d814417123c4cdd015373c0d4e
eocanas <elvalearns@outlook.com>
2015-04-19 15:35:07 -0700

    Assignment1
    First try

--- PA1_template.R
+++ PA1_template.R
@@ -1,20 +1,10 @@
----
-title: "assigment1"
-date: "Sunday, April 19, 2015"
-output: html_document
----
+## Libraries
 
-1. Setup of libraries  
-
-```{r echo=TRUE}
 library(ggplot2)
-library(RCurl)
-```
 
-2. Two functions were used for the calculations, functions usually are integrated in separated files.
-Setup of Functions:  
 
-```{r echo=TRUE}
+## Functions: 
+
 ## Function to replace each NA value with the mean value of its 5-minute interval
 fillFunc <- function(steps, interval) {
         filled <- NA
@@ -34,38 +24,22 @@ dayDef <- function(date) {
         else
                 stop("Invalid entry")
 }
-```
 
-The data is downloaded, unizipped and inserted into a data.frame:
-
-```{r echo=TRUE}
-download.file(url = "http://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip", destfile = "data.zip",mode = "wb")
+## Handling data
+download.file(url = "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip", destfile = "data.zip", method = "curl")
 unzip("data.zip", overwrite = T)
 data<-read.csv("activity.csv", header=T, colClasses="character")
-```
 
-The clases of the columns were modified:
-
-```{r echo=TRUE}
 ## Preparation of data.frame
 data$date<-as.Date(data$date, format="%Y-%m-%d")
 data$steps<-as.numeric(data$steps)
 data$interval<-as.numeric(data$interval)
-```
 
-## Question 1: What is mean total number of steps taken per day?
-
-Removing NA values:
-
-```{r echo=TRUE}
 ## NA omision
 data2<-data[!is.na(data$steps),]
-```
 
-Histogram using lapply to show the answer to the question:
-
-```{r echo=TRUE}
 ## Make a histogram of the total number of steps taken each day
+
 x<-lapply(split(data2$steps,data2$date), sum)
 x<-stack(x)
 x$ind<-as.Date(x$ind)
@@ -74,82 +48,49 @@ hist(x$values,
      main="Steps per day", 
      xlab="Steps", 
      ylab="Frequency")
-```
 
-Result of the mean and the median:
-
-```{r echo=TRUE}
 ## Calculation of mean and median 
 stepsPerDay <- aggregate(steps ~ date, data2, sum)$steps
 mean(stepsPerDay)
 median(stepsPerDay)
-```
 
-## Question 2: What is the average daily activity pattern?
-
-Making of a time series plot of the 5-minute interval (x-axis) and the average number of steps taken, averaged across all days (y-axis)
-
-```{r echo=TRUE}
 ## Average daily activity 
+
 averagedData <- aggregate(x=list(steps=data$steps), by=list(interval=data$interval),FUN=mean, na.rm=TRUE)
 ggplot(data=averagedData, aes(x=interval, y=steps)) + 
         geom_area() +xlab("Interval of 5 minutes each") +
-        ylab("Average of steps taken") 
-```
+        ylab("Average of steps taken")
 
-This is the interval that contains the maximun number of steps:
-
-```{r echo=TRUE}
 ## Interval with max value
 averagedData[which.max(averagedData$steps),]
-```
 
-## Imputing of averaged valued into missing values
 
-How many NA values are there?
-
-```{r echo=TRUE}
 ## NA values and result
 valueNA <- is.na(data$steps)
 table(valueNA)
-```
 
-Replacement of missing values:
-
-```{r echo=TRUE}
 # NA replacement
 data3 <- data
 data3$steps <- mapply(fillFunc, data3$steps, data3$interval)
-```
 
-Impact of the substitution of missing values can be seen in this plot:
-
-```{r echo=TRUE}
 # Plot with substituted elements
 stepsTotal <- tapply(data3$steps, data3$date, FUN=sum)
 qplot(stepsTotal, binwidth=1000, 
-      xlab="Steps per day, total", 
+      xlab="Steps per day, total",
       ylab="Frequency")
 mean(stepsTotal)
 median(stepsTotal)
-```
 
-## Question 3: Are there differences in activity patterns between weekdays and weekends?
-
-Aplication of day-defining function
-
-```{r echo=TRUE}
 ## Aplication of dayDef FUN
 data3$date <- as.Date(data3$date)
 data3$day <- sapply(data3$date, FUN=dayDef)
-```
 
-Throught this plot the answer to the question can be seen:
-
-```{r echo=TRUE}
 ## Answer to: Are there differences in activity patterns between weekdays and weekends?
 averagedData <- aggregate(steps ~ interval + day, data=data3, mean)
-ggplot(averagedData, aes(interval, steps)) + geom_area() + facet_grid(day ~ .) + 
+ggplot(averagedData, aes(interval, steps)) + 
+        geom_area() + 
+        facet_grid(day ~ .) + 
         xlab("5-minute interval") + 
         ylab("Number of steps")
-```
+
+
